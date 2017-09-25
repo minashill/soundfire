93c72f5449beadaa91d5b1dc6dc10b72a41b1496
minashill <minashill@yahoo.com>
2017-09-25 10:13:38 -0600

    updates to app.js and html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
-var LastfmAPI = require('lastfmapi');
-var lfm = new LastfmAPI({
-    'api_key' : '493f7a88030f6c7842e6bbf97ad8ce5e',
-    'secret' : '17e7b84f04cd561a2137a0d9f2776e9f'
-});
+   $('.carousel').carousel();
+// var LastfmAPI = require('lastfmapi');
+// var lfm = new LastfmAPI({
+//     'api_key' : '493f7a88030f6c7842e6bbf97ad8ce5e',
+//     'secret' : '17e7b84f04cd561a2137a0d9f2776e9f'
+// });
 
 /*Initialize LastFM Authorization*/
-// var token = window.location.search.split('?token=')[1];
-// LastFM API Key: 7ec54293b71ff780b78575e0bda44e26
+var token = window.location.search.split('?token=')[1];
+// LastFM API Key: '493f7a88030f6c7842e6bbf97ad8ce5e';
 if(token) {
  //display search
- document.getElementById('initsearch').style.display = "block";
- document.getElementById('login').style.display = "none";
- $('#bgvid').remove();
+ // document.getElementById('initsearch').style.display = "block";
+ // document.getElementById('login').style.display = "none";
+ // $('#bgvid').remove();
 } else {
   // display login
-  document.getElementById('login').style.display = "block";
+  // document.getElementById('login').style.display = "block";
   
 }
 /*End LastFM Authorization*/
@@ -40,6 +41,9 @@ function getTourSchedule(artist) {
 
          $("#artist-table tbody").append(row);//On the html display all the information
      } 
+      var h2 = $('<h2 class="header text_b" >');
+      h2.append('Live Shows');
+      $("#tour").html(h2);
 
         //$("#tour").text(artistData);
       });
@@ -48,7 +52,7 @@ function getTourSchedule(artist) {
 
 /* Initialize Artist Search through LastFM*/
 function lastFMSearch(artist) {
-  var lastFMSearchURL = "https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=" + artist + "&api_key=7ec54293b71ff780b78575e0bda44e26&format=json";
+  var lastFMSearchURL = "https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=" + artist + "&api_key=493f7a88030f6c7842e6bbf97ad8ce5e&format=json";
   // Can be used to strip for artist picture
   $.get(lastFMSearchURL)
     .done(function(response){
@@ -68,32 +72,33 @@ function lastFMSearch(artist) {
 /* End Artist Search through LastFM */
 
 function lastFMGetSimilarArtists(artist) {
-  var lastFMGetSimilarArtistsURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + artist + "&api_key=7ec54293b71ff780b78575e0bda44e26&format=json";
+  var lastFMGetSimilarArtistsURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + artist + "&api_key=493f7a88030f6c7842e6bbf97ad8ce5e&format=json";
 
-  $.get(lastFMGetSimilarArtistsURL)
+$.get(lastFMGetSimilarArtistsURL)
     .done(function(response){
       var div = $('<div>');
-      div.addClass('carousel');
-      var a = $('<a>');
-      a.addClass('carousel-item');
+      div.addClass('slider');
+      var ul = $('<ul>');
+      ul.addClass('slides');
       
 
       for(i = 0; i < 3; i++) {
-      var artName = response.similarartists.artist[i].name;
-      var artPic = response.similarartists.artist[i].image[2]['#text'];
+      var bandName = response.similarartists.artist[i].name;
+      var bandPic = response.similarartists.artist[i].image[2]['#text'];
         var img = $('<img>');
-        // var li = $('<li>');
-        // var h1 = $('<h3>');
-        h1.append(artName);
+        var li = $('<li>');
+        var h3 = $('<h3>');
+        h3.append(bandName);
         
-        img.attr('src', artPic);
-        img.attr("alt", artName);
-        h1.addClass('caption left-align');
+        img.attr('src', bandPic);
+        img.attr("alt", bandName);
+        li.attr("alt", bandName);
+        h3.addClass('caption left-align');
         // li.append(bandName);
         // p.attr("display", "none");
-        // li.append(h1);
-        a.append(img);
-        img.append('<h2 class="header text_b">')
+        li.append(h3);
+        li.append(img);
+        li.addClass('sugPic');
         // img.on('mouseenter', function(){
         //   p.slideToggle();
         // });
@@ -101,44 +106,67 @@ function lastFMGetSimilarArtists(artist) {
         // img.on('mouseleave', function() {
         //   p.slideToggle();
         // });
-        // ul.append(li);
+        ul.append(li);
       }
-      // div.append(ul);
+      var h2 = $('<h2 class="header text_b" >');
+      div.append(ul);
+      h2.append('Suggested Artists');
+      $("#suggestHead").html(h2);
       $("#suggest").html(div);
-      // $('.slider').slider();
+      $('.slider').slider();
+             
     });
 }
 
 
 function lastFMGetArtistInfo(artist) {
-	var lastFMGetArtistInfoURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=7ec54293b71ff780b78575e0bda44e26&format=json";
+	var lastFMGetArtistInfoURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=493f7a88030f6c7842e6bbf97ad8ce5e&format=json";
 
 	$.get(lastFMGetArtistInfoURL)
 		.done(function(response){
 			var artInfo = response.artist.bio.summary;
-			var p = $('<p>');
+			var p = $('<p> <i class="mdi-navigation-close right">');
 			p.append(artInfo);
 			$('#bio').html(p);
 		})
 }
-
-$(".searchBar").on("submit", function(event) {
-  var artist = $(this).find('input').val().trim();
+$('.sugPic').on('click', function(event) {
+  var artist = $(this).attr("alt");
 
   event.preventDefault();
 
   $("#artist-table tbody").empty();
-
-  document.getElementById('header').style.display = "block";
-  document.getElementById('results').style.display = "block";
-  document.getElementById('initsearch').style.display = "none";
 
   getTourSchedule(artist);
   lastFMSearch(artist);
   lastFMGetSimilarArtists(artist);  
   lastFMGetArtistInfo(artist);
 
+
 });
+
+$(".searchBar").on("submit", function(event) {
+  var artist = $(this).find('input').val().trim();
+
+  event.preventDefault();
+   // window.location = $('#work');
+
+
+  $("#artist-table tbody").empty();
+
+  // document.getElementById('header').style.display = "block";
+  // document.getElementById('results').style.display = "block";
+  // document.getElementById('initsearch').style.display = "none";
+
+  getTourSchedule(artist);
+  lastFMSearch(artist);
+  lastFMGetSimilarArtists(artist);  
+  lastFMGetArtistInfo(artist);
+  $("#work").css("display", "block");
+
+
+});
+
 
 
 
--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 
 if(isset($_POST['email'])) {
 
-    $email_to = "YOUR EMAIL ID GOES HERE";
+    $email_to = "minashill@yahoo.com";
  
     $email_subject = "Website Contact";
 
