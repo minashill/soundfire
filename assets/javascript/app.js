a43aad9cdbf49a8566d7110eca1a25cd7fdb153c
minashill <minashill@yahoo.com>
2017-10-11 19:29:50 -0600

    added venue search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,7 +23,7 @@ if(token) {
 
 /*Initialize TourSchedule for Bands in Town*/
 function getTourSchedule(artist) {
-  var bandsInTownURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=adio";
+  var bandsInTownURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=soundFire";
 
   $.get(bandsInTownURL)
       .done(function(response){
@@ -49,6 +49,60 @@ function getTourSchedule(artist) {
       });
 }
 /*End TourSchedule via Bands in Town*/
+
+/*Venue Search*/
+function getVenue(venue) {
+  var songKickURL = `http://api.songkick.com/api/3.0/search/venues.json?query=${venue}&apikey=YEZ681bUYQtJ1y2p`;
+  console.log(venue);
+
+  $.get(songKickURL)
+    .done(function(response){
+      var venueName = response.resultsPage.results.venue[0].displayName;
+      var venueWeb = response.resultsPage.results.venue[0].website;
+      var venueID = response.resultsPage.results.venue[0].id;
+      var venueCity = response.resultsPage.results.venue[0].city.displayName;
+      var venueState = response.resultsPage.results.venue[0].city.state.displayName;
+      var venueStreet = response.resultsPage.results.venue[0].street;
+      var venuePhone = response.resultsPage.results.venue[0].phone;
+      var venueCapacity = response.resultsPage.results.venue[0].capacity;
+      var h2 = $('<h2 class="header text_b" >');
+      h2.append(venueName);
+      console.log(venueName);
+      $("#ven").html(h2);
+      // resultsPage.results.event
+    console.log(venueID);
+
+  var songKickURL2 = `http://api.songkick.com/api/3.0/venues/${venueID}/calendar.json?apikey=YEZ681bUYQtJ1y2p`;
+  console.log(venueID);
+  console.log(songKickURL2);
+
+  $.get(songKickURL2)
+      .done(function(response){
+        for (var i = 0; i < response.resultsPage.results.event.length; i++) { //On the API, grab the first content of the array to the length of the array
+            var row1 = $('<tr>');// On the html table go to the row
+             row1.append(`<td>${response.resultsPage.results.event[i].displayName.slice(0,)}`);// On the row, the first item is the event name 
+             var d = response.resultsPage.results.event[i].start.datetime.slice(0,10).split('-');
+             var convDate1 = d[1]+'/'+d[2]+'/'+d[0];
+             row1.append('<td>'+convDate1);
+             // row.append('<td>' + response[i].datetime);//the second item is the date the gig will play
+             row1.append('<td>'+ response.resultsPage.results.event[i].location.city);//the city in which the venue is. Eg pepsi center is in Denver
+
+             row1.append('<td> <a href="'+ response.resultsPage.results.event[i].uri + '" target="_blank"> Buy Tickets');// Make the content here clickable
+
+
+         $("#venue-table tbody").append(row1);//On the html display all the information
+         console.log(row1);
+     } 
+      
+
+        //$("#tour").text(artistData);
+      });
+
+    });
+  }
+
+/*Venue Calendar*/
+
 
 /* Initialize Artist Search through LastFM*/
 function lastFMSearch(artist) {
@@ -130,6 +184,10 @@ function lastFMGetArtistInfo(artist) {
 			$('#bio').html(p);
 		})
 }
+
+// function spotifyArtistId(artist){
+//   var spotifyArtistIdURL = 
+// }
 //button actions
 
 $(".homeButton").on("click", function(event) {
@@ -140,6 +198,7 @@ $(".homeButton").on("click", function(event) {
   $("#venueSearch").css("display", "none");
   $("#citySearch").css("display", "none");
   $("#artistResults").css("display", "none");
+  $("#venueResults").css("display", "none");
   $("#contact").css("display", "none");
   
   location.href = "#artistSearch";
@@ -153,6 +212,7 @@ $(".artButton").on("click", function(event) {
   $("#artistSearch").css("display", "block");
   $("#index-banner").css("display", "none");
   $("#venueSearch").css("display", "none");
+  $("#venueResults").css("display", "none");
   $("#citySearch").css("display", "none");
   $("#contact").css("display", "none");
   
@@ -167,6 +227,7 @@ $(".venButton").on("click", function(event) {
   $("#index-banner").css("display", "none");
   $("#artistSearch").css("display", "none");
   $("#citySearch").css("display", "none");
+  $("#venueResults").css("display", "none");
   $("#artistResults").css("display", "none");
   $("#contact").css("display", "none");
   
@@ -182,6 +243,7 @@ $(".citButton").on("click", function(event) {
   $("#artistSearch").css("display", "none");
   $("#venueSearch").css("display", "none");
   $("#artistResults").css("display", "none");
+  $("#venueResults").css("display", "none");
   $("#contact").css("display", "none");
   
   location.href = "#citySearch";
@@ -197,6 +259,8 @@ $(".contButton").on("click", function(event) {
   $("#venueSearch").css("display", "none");
   $("#artistResults").css("display", "none");
   $("#citySearch").css("display", "none");
+  $("#venueResults").css("display", "none");
+
 
  
   
@@ -215,6 +279,7 @@ $(".searchBar").on("submit", function(event) {
    
 
   $("#artist-table tbody").empty();
+  $("#venue-table tbody").empty();
 
   // document.getElementById('header').style.display = "block";
   // document.getElementById('results').style.display = "block";
@@ -225,8 +290,30 @@ $(".searchBar").on("submit", function(event) {
   lastFMGetSimilarArtists(artist);  
   lastFMGetArtistInfo(artist);
   $("#artistResults").css("display", "block");
+  $("#venueResults").css("display", "none");
 
   location.href = "#artistResults"; 
+
+});
+
+$(".vsearchBar").on("submit", function(event) {
+  var venue = $(this).find('input').val().trim();
+
+  event.preventDefault();
+  // setTimout(function() {)
+   
+
+  $("#venue-table tbody").empty();
+  $("#ven").empty();
+  // document.getElementById('header').style.display = "block";
+  // document.getElementById('results').style.display = "block";
+  // document.getElementById('initsearch').style.display = "none";
+  
+  getVenue(venue);
+  $("#venueResults").css("display", "block");
+  $("#artistResults").css("display", "none");
+
+  location.href = "#venueResults"; 
 
 });
 
